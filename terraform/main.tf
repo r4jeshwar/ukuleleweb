45d4adcca24b8dde25790bdeab6114791c849915
Rajeshwar R <100279715+r4jeshwar@users.noreply.github.com>
2023-05-22 13:24:15 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "google_vpc_access_connector" "connector" {
-  name          = "vpc-con"
+  name          = "ukuleleweb-vpc-connector"
   ip_cidr_range = "10.8.0.0/28"
   network       = "default"
 }
